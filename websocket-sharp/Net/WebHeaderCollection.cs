e0c0aa50fdca3dd945d91aea614dbc42ece047b9
sta <sta.blockhead@gmail.com>
2026-03-03 16:40:22 +0900

    [Modify] Edit it

--- websocket-sharp/Net/WebHeaderCollection.cs
+++ websocket-sharp/Net/WebHeaderCollection.cs
@@ -1376,7 +1376,7 @@ namespace WebSocketSharp.Net
     /// A <see cref="string"/> that holds the value of the specified header.
     /// </returns>
     /// <param name="index">
-    /// An <see cref="int"/> that specifies the zero-based index of the header
+    /// An <see cref="int"/> that specifies the zero-based index of a header
     /// to get.
     /// </param>
     /// <exception cref="ArgumentOutOfRangeException">
